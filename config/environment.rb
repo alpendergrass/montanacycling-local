c68f4d2c3ad6815474fd813f062471a9b84be387
Al Pendergrass <al.pendergrass@gmail.com>
2009-06-09 10:07:52 -0600

    additional association-specific settings

--- config/environment.rb
+++ config/environment.rb
@@ -10,8 +10,9 @@ ASSOCIATION.show_only_association_sanctioned_races_on_calendar = false
 ASSOCIATION.show_events_sponsoring_team = true
 ASSOCIATION.show_events_velodrome = false
 ASSOCIATION.rental_numbers = 51..99 if RAILS_ENV == 'test'
-ASSOCIATION.usac_region = "North West"  #mbratodo: is MT in NW region?
+ASSOCIATION.usac_region = "Mountain"
+ASSOCIATION.usac_results_format = true
+ASSOCIATION.default_sanctioned_by = "USA Cycling" #mbratodo I was using DEFAULT_SANCTIONING_ORGANIZATION
 
 SANCTIONING_ORGANIZATIONS = ["UCI", "USA Cycling"] unless defined?(SANCTIONING_ORGANIZATIONS)
-#DEFAULT_SANCTIONING_ORGANIZATION = "USA Cycling" unless defined?(DEFAULT_SANCTIONING_ORGANIZATION)
 
