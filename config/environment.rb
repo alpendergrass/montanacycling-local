e88dcbf583f78cd7094a39642ac779c6d7aa4d5c
Scott Willson <scott@butlerpress.com>
2009-06-21 21:19:38 -0700

    Always show Event Team sponsor, so no need for config option

--- config/environment.rb
+++ config/environment.rb
@@ -7,7 +7,6 @@ ASSOCIATION.short_name = 'MBRA'
 ASSOCIATION.state = 'MT'
 ASSOCIATION.masters_age = 40
 ASSOCIATION.show_only_association_sanctioned_races_on_calendar = false
-ASSOCIATION.show_events_sponsoring_team = true
 ASSOCIATION.show_events_sanctioning_org_event_id = true
 ASSOCIATION.show_events_velodrome = false
 ASSOCIATION.rental_numbers = 51..99 if RAILS_ENV == 'test'
