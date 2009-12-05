d099732b816861319f3353117ccd012a579f9a8a
Scott Willson <sw@Al.local>
2009-12-04 21:19:45 -0800

    Log to syslog

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,3 +1,8 @@
+require 'syslog_logger'
+syslog = SyslogLogger.new("mrbra_rails")
+syslog.level = Logger::INFO
+@config.logger = syslog
+
 REGIONAL_SITE_LINKS = "/articles/12"
 RAILS_HOST  = 'www.montanacycling.net'
 STATIC_HOST = 'www.montanacycling.net'
