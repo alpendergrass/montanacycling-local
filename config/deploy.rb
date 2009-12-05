f44f30a5c5186d8eb862995b04650fea462310fd
Scott Willson <sw@Al.local>
2009-12-04 20:57:26 -0800

    Add symlinks to static content

--- config/deploy.rb
+++ config/deploy.rb
@@ -2,3 +2,12 @@ set :application, "mbra"
 
 role :app, "69.30.43.18"
 role :db, "69.30.43.18", :primary => true
+
+after "deploy", "deploy:link_to_static_content"
+
+namespace :deploy do
+  task :link_to_static_content do
+    run "ln -s /usr/local/www/mbra/archive public/archive"
+    run "ln -s /usr/local/www/mbra/documents public/documents"
+  end
+end
